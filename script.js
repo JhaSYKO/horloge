7dae1712c08a2b39bd7b982cb606714bef2198ef
JhaSYKO <tematahotoa.tini@gmail.com>
2019-12-24 09:46:55 -1000

    upadte style

--- script.js
+++ script.js
@@ -9,9 +9,11 @@ function setClock() {
     const secondsRatio = currentDate.getSeconds() / 60
     const minutesRatio = (secondsRatio + currentDate.getMinutes()) / 60
     const hoursRatio = (minutesRatio + currentDate.getHours()) / 12
+
     setRotation(secondHand, secondsRatio)
     setRotation(minuteHand, minutesRatio)
     setRotation(hourHand, hoursRatio)
+    
 }
 
 function setRotation(element,  rotationRatio) {
