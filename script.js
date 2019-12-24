e61a1cef392a57c23c57f6d143df468d8a745427
JhaSYKO <tematahotoa.tini@gmail.com>
2019-12-24 13:15:23 -1000

    remove console.log

--- script.js
+++ script.js
@@ -7,13 +7,13 @@ const secondHand = document.querySelector('[data-second-hand]')
 function setClock() {
     const currentDate = new Date()
     const secondsRatio = currentDate.getSeconds() / 60
+    const retrieveSecond = parseInt(secondsRatio)
     const minutesRatio = (secondsRatio + currentDate.getMinutes()) / 60
     const hoursRatio = (minutesRatio + currentDate.getHours()) / 12
 
     setRotation(secondHand, secondsRatio)
     setRotation(minuteHand, minutesRatio)
     setRotation(hourHand, hoursRatio)
-    console.log(secondsRatio*360)
 
 }
 
